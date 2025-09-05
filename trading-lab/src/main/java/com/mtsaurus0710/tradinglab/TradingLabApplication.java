88747907a48277348bbe684a4009a73b0fef8504
MTsuaRus <qksrurwnsql@naver.com>
2025-09-05 13:09:14 +0900

    first commit

---
+++ trading-lab/src/main/java/com/mtsaurus0710/tradinglab/TradingLabApplication.java
@@ -0,0 +1,13 @@
+package com.mtsaurus0710.tradinglab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingLabApplication.class, args);
+	}
+
+}
---
+++ trading-lab/src/test/java/com/mtsaurus0710/tradinglab/TradingLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtsaurus0710.tradinglab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
